09815b3621238a1ad2b8be2ad9fd59d64e119470
James Taylor <james.taylor@gatech.edu>
2022-04-21 22:59:00 -0400

    modify some formatting and default configs, as well as removing some unnecessary comments

--- neat/experiments/chess/config.py
+++ neat/experiments/chess/config.py
@@ -1,16 +1,21 @@
-import torch
 import logging
-import torch.nn as nn
 import pickle
+
 import numpy as np
+import torch
+import torch.nn as nn
 from torch import autograd
+
 from neat.phenotype.feed_forward import FeedForwardNet
+
 
 logger = logging.getLogger(__name__)
 
 class ChessConfig:
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # DEVICE = "cuda:0"
+    print(f"device: {DEVICE}")
+    np_rng = np.random.default_rng(13)
     VERBOSE = True
 
     NUM_INPUTS = 8
@@ -22,37 +27,38 @@ class ChessConfig:
 
     FITNESS_THRESHOLD = 1970
 
-    POPULATION_SIZE = 100
+    POPULATION_SIZE = 150
     NUMBER_OF_GENERATIONS = 150
     SPECIATION_THRESHOLD = 3.0
 
     CONNECTION_MUTATION_RATE = 0.80
     CONNECTION_PERTURBATION_RATE = 0.90
-    ADD_NODE_MUTATION_RATE = 0.0
+    ADD_NODE_MUTATION_RATE = 0.03
     ADD_CONNECTION_MUTATION_RATE = 0.5
 
     CROSSOVER_REENABLE_CONNECTION_GENE_RATE = 0.25
 
     # Top percentage of species to be saved before mating
     PERCENTAGE_TO_SAVE = 0.30
+
     print("loading chess data")
-    print(DEVICE)
     with open("../data/KQK/indices", "rb") as f:
-        tensors = pickle.load(f)
-        # print(tensors)
+        data = pickle.load(f)
+
     inputs_list = []
     outputs_list = []
-    np.random.shuffle(tensors)
-    tensors = tensors[:2000]
-    for tensor in tensors:
-        tensor_in = np.array(tensor[0])
-        # condensed_input = np.argmax(tensor_in.reshape(4, 64), axis=1)
-        inputs_list.append(tensor_in)
-        outputs_list.append(tensor[1])
+    np_rng.shuffle(data)
+    data_proportion = int(np.sqrt(len(data)))
+    data = data[:data_proportion]
+    for xy in data:
+        # xy[0] should already be a numpy array
+        inputs_list.append(xy[0])
+        outputs_list.append(xy[1])
 
     inputs = list(map(lambda s: autograd.Variable(torch.Tensor([s])), inputs_list))
 
     targets = list(map(lambda s: autograd.Variable(torch.Tensor([s])), outputs_list))
+
 
     def fitness_fn(self, genome):
         fitness = 2000  # Max fitness for XOR
@@ -61,21 +67,20 @@ class ChessConfig:
         phenotype.to(self.DEVICE)
         criterion = nn.MSELoss()
         num_inputs = len(self.inputs)
-        for input, target in zip(self.inputs, self.targets):  # 4 training examples
+
+        for input, target in zip(self.inputs, self.targets):
             input, target = input.to(self.DEVICE), target.to(self.DEVICE)
 
             pred = phenotype(input)
-            # print(pred)
-            # print(target)
-            loss = (float(pred) - float(target)) ** 2
-            loss = float(loss)
-            # loss = criterion(pred, target)
+            # loss = (float(pred) - float(target)) ** 2
+            # loss = float(loss)
+            loss = criterion(pred, target)
             # logger.info("Loss: {}".format(loss))
             fitness -= loss
             # logger.info("Fitness: {}".format(fitness))
-        # fitness = fitness / num_inputs
-        # logger.info("Fitness: {}".format(fitness))
+
         return fitness
+
 
     def get_preds_and_labels(self, genome):
         phenotype = FeedForwardNet(genome, self)
@@ -83,7 +88,7 @@ class ChessConfig:
 
         predictions = []
         labels = []
-        for input, target in zip(self.inputs, self.targets):  # 4 training examples
+        for input, target in zip(self.inputs, self.targets):
             input, target = input.to(self.DEVICE), target.to(self.DEVICE)
 
             predictions.append(float(phenotype(input)))
